d08c4f5f7e86f01a32b752763aa858431b4fa787
João Paulo Correa <jopa.correa27@gmail.com>
2025-05-10 17:41:32 -0600

    Adjust index.js nova frase para Deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Curso.dev 👨🏻‍💻✨</h1>
+    return (
+    <div>
+        <h1>Curso.dev 👨🏻‍💻✨</h1>
+        <h3>Olá mundo😘 </h3>
+    </div>
+) 
 }
 
 export default Home;
